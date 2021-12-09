cb175b7ae6e002a9f90d37fe2cc6e751c596c3f3
Asrafil <asrafilr@gmail.com>
2021-12-09 18:05:40 +0700

    Tes Cashy done

--- tescashy/lib/pages/message_page.dart
+++ tescashy/lib/pages/message_page.dart
@@ -1,11 +1,243 @@
 import 'package:flutter/material.dart';
+import 'package:tescashy/pages/home_page.dart';
 import 'package:tescashy/theme.dart';
 
 class MessagePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => HomePage()));
+          },
+          backgroundColor: greenColor,
+          child: Icon(
+            Icons.arrow_back,
+            size: 28,
+          ),
+        ),
         backgroundColor: lightBackground,
-        body: SafeArea(child: Text("sukses pindah page")));
+        body: SafeArea(
+            child: Column(
+          children: [
+            Container(
+              height: 115,
+              decoration: BoxDecoration(
+                  color: whiteColor,
+                  borderRadius:
+                      BorderRadius.vertical(bottom: Radius.circular(40))),
+              child: Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/group1.png',
+                      height: 55,
+                      width: 55,
+                    ),
+                    SizedBox(
+                      width: 12,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Jakarta Fair",
+                          style: titleTextStyle,
+                        ),
+                        Text(
+                          "14,209 members",
+                          style: subtitleTextStyle,
+                        )
+                      ],
+                    ),
+                    Spacer(),
+                    Image.asset('assets/images/call_btn.png')
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Image.asset(
+                        'assets/images/friend1.png',
+                        height: 40,
+                        width: 40,
+                      ),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: messageBackground,
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                topRight: Radius.circular(20),
+                                bottomRight: Radius.circular(20))),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 11.0,
+                            horizontal: 20.0,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "How are you guys",
+                                style: titleTextStyle,
+                              ),
+                              Text(
+                                "2:30",
+                                style: subtitleTextStyle,
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Image.asset(
+                        'assets/images/friend2.png',
+                        height: 40,
+                        width: 40,
+                      ),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: messageBackground,
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                topRight: Radius.circular(20),
+                                bottomRight: Radius.circular(20))),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 11.0,
+                            horizontal: 20.0,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Find here :P",
+                                style: titleTextStyle,
+                              ),
+                              Text(
+                                "3:11",
+                                style: subtitleTextStyle,
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            color: whiteColor,
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                topRight: Radius.circular(20),
+                                bottomLeft: Radius.circular(20))),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 11.0,
+                            horizontal: 20.0,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Text(
+                                "Thinking about how to deal \nwith this client from hell...",
+                                style: titleTextStyle,
+                                textAlign: TextAlign.end,
+                              ),
+                              Text(
+                                "22:08",
+                                style: subtitleTextStyle,
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      Image.asset(
+                        'assets/images/friend3.png',
+                        height: 40,
+                        width: 40,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Image.asset(
+                        'assets/images/friend2.png',
+                        height: 40,
+                        width: 40,
+                      ),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: messageBackground,
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                topRight: Radius.circular(20),
+                                bottomRight: Radius.circular(20))),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 11.0,
+                            horizontal: 20.0,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Love them",
+                                style: titleTextStyle,
+                              ),
+                              Text(
+                                "23:11",
+                                style: subtitleTextStyle,
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        )));
   }
 }
